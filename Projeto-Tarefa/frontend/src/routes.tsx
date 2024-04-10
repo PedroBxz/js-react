5f0c25b3a0d18b955439a48d86b685e619c260a1
Pedro Silva <spedru07@gmail.com>
2024-04-10 20:29:39 -0300

    adi

---
+++ Projeto-Tarefa/frontend/src/routes.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+import Tasks from "./pages/tasks";
+
+const AppRoutes: React.FC = () => {
+return (
+<Routes>
+<Route path="/" element={<Home />} />
+<Route path="/tarefas" element={<Tasks />} />
+</Routes>
+);
+};
+
+export default AppRoutes;
+
